03aee40b02bb8421be4e06cbce830e79044e7e93
Hoangyd <161820951+Hoangyd@users.noreply.github.com>
2024-03-14 16:44:58 +0700

    quanco

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,27 @@
- #include "mapco.h"
+#include "mapco.h"
 #include "../include/SDL2/SDL.h"
 #include "../include/SDL2/SDL_image.h"
+#include <iostream>
 int main(int argc, char* args[]) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cerr << "SDL không thể khởi tạo! Lỗi SDL: " << SDL_GetError() << std::endl;
         return 1;
     }
-
     gWindow = SDL_CreateWindow("IQCHESS", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (gWindow == nullptr) {
         std::cerr << "Không thể tạo cửa sổ! Lỗi SDL: " << SDL_GetError() << std::endl;
         return 1;
     }
-
     gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
     if (gRenderer == nullptr) {
         std::cerr << "Không thể tạo renderer! Lỗi SDL: " << SDL_GetError() << std::endl;
         return 1;
     }
-
     rook1Texture = loadTexture("image/conco-fotor-202403121127(1).png");
     rook2Texture = loadTexture("image/conco-fotor-202403121127(1).png");
-
     bool quit = false;
     SDL_Event e;
-
+    
     while (!quit) {
         while (SDL_PollEvent(&e) != 0) {
             if (e.type == SDL_QUIT) {
@@ -34,34 +31,33 @@ int main(int argc, char* args[]) {
                 SDL_GetMouseState(&x, &y);
                 int col = x / SQUARE_SIZE;
                 int row = y / SQUARE_SIZE;
-
-                if (selectedRook == 0) {
-                    rook1X = col;
-                    rook1Y = row;
-                    selectedRook = -1; // Bỏ chọn quân xe sau khi di chuyển
-                } else if (selectedRook == 1) {
-                    rook2X = col;
-                    rook2Y = row;
-                    selectedRook = -1; // Bỏ chọn quân xe sau khi di chuyển
-                } else {
-                    if (col == rook1X && row == rook1Y) {
-                        selectedRook = 0; // Chọn Quân xe 1
-                    } else if (col == rook2X && row == rook2Y) {
-                        selectedRook = 1; // Chọn Quân xe 2
+                // Kiểm tra xem ô được chọn có thể di chuyển hay không
+                if (isValidMove(col, row)) {
+                    if (selectedRook == 0) {
+                        rook1X = col;
+                        rook1Y = row;
+                        selectedRook = -1; // Bỏ chọn quân xe sau khi di chuyển
+                    } else if (selectedRook == 1) {
+                        rook2X = col;
+                        rook2Y = row;
+                        selectedRook = -1; // Bỏ chọn quân xe sau khi di chuyển
+                    } else {
+                        if (col == rook1X && row == rook1Y) {
+                            selectedRook = 0; // Chọn Quân xe 1
+                        } else if (col == rook2X && row == rook2Y) {
+                            selectedRook = 1; // Chọn Quân xe 2
+                        }
                     }
                 }
+               render();
             }
-
-            render();
         }
     }
-
+   
     SDL_DestroyRenderer(gRenderer);
     SDL_DestroyWindow(gWindow);
     gWindow = nullptr;
     gRenderer = nullptr;
-
     SDL_Quit();
-
     return 0;
 }
--- src/mapco.cpp
+++ src/mapco.cpp
@@ -1,5 +1,5 @@
-#include "mapco.h"
 
+#include "mapco.h"
 const int SCREEN_WIDTH = 700;
 const int SCREEN_HEIGHT = 700;
 const int SQUARE_SIZE = 100;
@@ -12,51 +12,43 @@ int rook1Y = 0;
 int rook2X = 4;
 int rook2Y = 0;
 int selectedRook = -1;
-
 SDL_Texture* loadTexture(const std::string& path) {
     SDL_Surface* surface = IMG_Load(path.c_str());
     if (!surface) {
         std::cerr << "Failed to load image: " << IMG_GetError() << std::endl;
         return nullptr;
     }
-
     SDL_Texture* texture = SDL_CreateTextureFromSurface(gRenderer, surface);
     if (!texture) {
         std::cerr << "Failed to create texture from surface: " << SDL_GetError() << std::endl;
     }
-
     SDL_FreeSurface(surface);
     return texture;
 }
-
 void drawTexture(SDL_Texture* texture, int x, int y) {
-    int newWidth = SQUARE_SIZE * 5/ 6; // Kích thước mới cho quân cờ (5/6 so với SQUARE_SIZE)
-    int newHeight = SQUARE_SIZE * 5/ 6;
-
+    int newWidth = SQUARE_SIZE * 4/ 5; // Kích thước mới cho quân cờ (5/6 so với SQUARE_SIZE)
+    int newHeight = SQUARE_SIZE * 4/ 5;
     SDL_Rect destRect = { x * SQUARE_SIZE + (SQUARE_SIZE - newWidth) / 2, y * SQUARE_SIZE + (SQUARE_SIZE - newHeight) / 2, newWidth, newHeight };
     SDL_RenderCopy(gRenderer, texture, NULL, &destRect);
 }
 
-
 void drawBoard() {
     for (int y = 0; y < 8; ++y) {
         for (int x = 0; x < 8; ++x) {
-            if (!((x == 0 && y == 0) || (x == 1 && y == 0) || (x == 0 && y == 1) || (x == 1 && y == 1) ||
-                  (x == 6 && y == 0) || (x == 6 && y == 1) || (x == 5 && y == 1) || (x == 5 && y == 0) ||
-                  (x == 0 && y == 5) || (x == 0 && y == 6) || (x == 1 && y == 5) || (x == 1 && y == 6) ||
-                  (x == 5 && y == 5) || (x == 5 && y == 6) || (x == 6 && y == 5) || (x == 6 && y == 6))) {
-
+            bool isInvalidSquare = ((x == 0 && y == 0) || (x == 1 && y == 0) || (x == 0 && y == 1) || (x == 1 && y == 1) ||
+                                    (x == 6 && y == 0) || (x == 6 && y == 1) || (x == 5 && y == 1) || (x == 5 && y == 0) ||
+                                    (x == 0 && y == 5) || (x == 0 && y == 6) || (x == 1 && y == 5) || (x == 1 && y == 6) ||
+                                    (x == 5 && y == 5) || (x == 5 && y == 6) || (x == 6 && y == 5) || (x == 6 && y == 6));
+            if (!isInvalidSquare) {
                 int centerX = x * SQUARE_SIZE + SQUARE_SIZE / 2;
                 int centerY = y * SQUARE_SIZE + SQUARE_SIZE / 2;
                 int radius1 = SQUARE_SIZE /4 ; // Bán kính vòng ngoài
-                int radius2 = SQUARE_SIZE /8; // Bán kính vòng trong
-
+                int radius2 = SQUARE_SIZE /12; // Bán kính vòng trong
                 if ((x + y) % 2 == 0) {
                     SDL_SetRenderDrawColor(gRenderer, 112, 73, 25, 255); // Màu ô sáng
                 } else {
                     SDL_SetRenderDrawColor(gRenderer, 112, 73, 25, 255); // Màu ô tối
                 }
-
                 // Vẽ ô cờ nhỏ
                 for (int i = 0; i < 360; ++i) {
                     float angle = i * M_PI / 180;
@@ -73,13 +65,19 @@ void drawBoard() {
         }
     }
 }
-
-
-
+// Hàm kiểm tra ô có thể di chuyển
+    bool isValidMove(int x, int y) {
+        if ((x == 0 && y == 0) || (x == 1 && y == 0) || (x == 0 && y == 1) || (x == 1 && y == 1) ||
+            (x == 6 && y == 0) || (x == 6 && y == 1) || (x == 5 && y == 1) || (x == 5 && y == 0) ||
+            (x == 0 && y == 5) || (x == 0 && y == 6) || (x == 1 && y == 5) || (x == 1 && y == 6) ||
+            (x == 5 && y == 5) || (x == 5 && y == 6) || (x == 6 && y == 5) || (x == 6 && y == 6)) {
+            return false; // Ô không thể di chuyển tới
+        }
+        return true; // Ô có thể di chuyển tới
+    }
 void render() {
-    SDL_SetRenderDrawColor(gRenderer, 240, 167, 66, 255);
-    SDL_RenderClear(gRenderer);
-
+  //  SDL_SetRenderDrawColor(gRenderer, 240, 167, 66, 225);
+    //SDL_RenderClear(gRenderer);
     // Tải và vẽ background
     SDL_Texture* backgroundTexture = loadTexture("image/bancogo.jpg");
     if (backgroundTexture) {
@@ -87,12 +85,10 @@ void render() {
         SDL_RenderCopy(gRenderer, backgroundTexture, NULL, &destRect);
         SDL_DestroyTexture(backgroundTexture); // Giải phóng texture sau khi sử dụng
     }
-
     drawBoard();
     
     drawTexture(rook1Texture, rook1X, rook1Y);
     drawTexture(rook2Texture, rook2X, rook2Y);
-
     SDL_RenderPresent(gRenderer);
 }
 
